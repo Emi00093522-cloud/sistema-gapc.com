0444ab8c541709d46365bf9b88e29628ccde79ed
ek-mtz07 <00032022@uca.edu.sv>
2025-11-11 19:48:11 -0600

    Delete modulos/config/ahorro.py

--- modulos/config/ahorro.py
+++
@@ -1,6 +0,0 @@
-# modulos/ahorro.py
-import streamlit as st
-def mostrar_ahorro():
- # Aquí se muestra "Hola Mundo"
- st.title("Hola Mundo")
- st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
