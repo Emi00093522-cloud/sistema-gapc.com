28ca660484f6ad083f609995eb8bd1594582d564
ek-mtz07 <00032022@uca.edu.sv>
2025-11-11 19:37:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from modulos.ahorro import mostrar_ahorro
+# from modulos.ahorro import mostrar_ahorro
 from modulos.login import login
 
 # Llamamos a la función mostrar_ahorro para mostrar el mensaje en la app
-mostrar_ahorro()
+# mostrar_ahorro()
 login()
